69688ba10ee9c540d25cdb959b535e473019a4d4
unknown <danmaggot2@gmail.com>
2024-06-28 17:49:47 -0300

    :seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Pagination with Next.js",
-  description: "Exemplo de paginação com Next.js, mostrando como implementar paginação de forma eficiente.",
+  description:
+    "Exemplo de paginação com Next.js, mostrando como implementar paginação de forma eficiente.",
   keywords: "pagination, nextjs, react, desenvolvimento web",
   author: "Daniel Rocha",
-  viewport: "width=device-width, initial-scale=1.0",
-  robots: "index, follow",
+  robots: "index, follow"
 };
 
 export default function RootLayout({ children }) {
